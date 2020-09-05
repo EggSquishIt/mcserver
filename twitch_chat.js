e4990c681c41de64209bc57168cc043ddb0cf4a8
EggSquishIt <official@eggsquishit.net>
2020-09-05 15:34:22 +0200

    Added support to twitch_chat.js for logging in as a user.

--- twitch_chat.js
+++ twitch_chat.js
@@ -1,15 +1,22 @@
 const tmi = require('tmi.js');
+const fs = require('fs');
 
-const client = new tmi.Client({
-	connection: {
-		secure: true,
-		reconnect: true
-	},
-	channels: [ 'EggSquishIt' ]
-});
+fs.readFile("twitch_identity.json", "utf8", function(err, data) {
+	var identity = JSON.parse(data);
 
-client.connect();
+	var opts = {
+		identity: identity,
+		connection: {
+			secure: true,
+			reconnect: true
+		},
+		channels: [ 'EggSquishIt' ]
+	};
+	const client = new tmi.Client(opts);
 
-client.on('message', function(channel, tags, message, self) {
-	console.log(`${tags['display-name']}: ${message}`);
+	client.connect();
+
+	client.on('message', function(channel, tags, message, self) {
+		console.log(`${tags['display-name']}: ${message}`);
+	});
 });
