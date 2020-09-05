e42c9600a2cd6335182e2af1f3f08561a3937528
EggSquishIt <official@eggsquishit.net>
2020-09-05 15:56:09 +0200

    Added say and whisper commands to twitch client.

--- twitch_chat.js
+++ twitch_chat.js
@@ -1,5 +1,6 @@
 const tmi = require('tmi.js');
 const fs = require('fs');
+const readline = require('readline');
 
 fs.readFile("twitch_identity.json", "utf8", function(err, data) {
 	var identity = JSON.parse(data);
@@ -19,4 +20,28 @@ fs.readFile("twitch_identity.json", "utf8", function(err, data) {
 	client.on('message', function(channel, tags, message, self) {
 		console.log(`${tags['display-name']}: ${message}`);
 	});
+
+	var con = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: false
+	});
+
+	con.on("line", function(line) {
+		var re = /^([^ ]*) ([^ ]*) (.*)$/
+		var match = line.match(re);
+		var cmd = match[1];
+		var target = match[2];
+		var msg = match[3];
+
+		switch(cmd) {
+			case "say":
+				client.say(target, msg);
+				break;
+			case "whisper":
+				client.whisper(target, msg);
+				break;
+		}
+	});
+
 });
